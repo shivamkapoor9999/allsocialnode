cce6adf6dfa0656f594902039d84d6e18d9d985a
Shivam Kapoor <shivamkapoor9999@gmail.com>
2018-01-28 18:30:08 +0530

    node

--- chat_app.js
+++ chat_app.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-http.listen(5000, function(){
+http.listen(process.env.PORT || 5000, function(){
   console.log('listening on *:5000');
 });
    
